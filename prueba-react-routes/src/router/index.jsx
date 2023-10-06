d7bec637c65f3b577013255aa25816bac6872bb3
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2023-10-06 10:35:38 -0600

    the commentes were womeved

--- prueba-react-routes/src/router/index.jsx
+++ prueba-react-routes/src/router/index.jsx
@@ -6,9 +6,6 @@ const Login = lazy(() => import('../components/pages/Login'));
 const SignUp = lazy(() => import('../components/pages/SignUp'));
 const Home = lazy(() => import('../components/pages/Home'));
 const NotFound = lazy(() => import('../components/pages/NotFound'))
-// import Home from "../components/pages/Home";
-// import Login from "../components/pages/Login";
-// import SignUp from "../components/pages/SignUp";
 
 export const router = createBrowserRouter([
     {
