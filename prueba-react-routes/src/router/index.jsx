adc7b50eb2b55db582b316cfa4b6b4c958a49ffc
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2023-10-06 14:12:12 -0600

    the comments were deleted

--- prueba-react-routes/src/router/index.jsx
+++ prueba-react-routes/src/router/index.jsx
@@ -1,6 +1,5 @@
-import { lazy, Suspense } from "react";
+import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
-import LoadingSpinner from "../components/LoadingSpinner";
 
 const Login = lazy(() => import('../components/pages/Login'));
 const SignUp = lazy(() => import('../components/pages/SignUp'));
@@ -8,17 +7,6 @@ const Home = lazy(() => import('../components/pages/Home'));
 const NotFound = lazy(() => import('../components/pages/NotFound'))
 
 export const router = createBrowserRouter([
-    // {
-        // path: "/", element: <Suspense fallback={<LoadingSpinner />}><Home /></Suspense>, errorElement: <NotFound />
-    // },
-    // {
-        // path: "/Login", element: <Suspense fallback={<LoadingSpinner/>}> < Login/> </Suspense> , errorElement: <NotFound />
-    // },
-    // {
-        // path: "/SignUp", element: <Suspense fallback={<LoadingSpinner/>}> <SignUp /> </Suspense> , errorElement: <NotFound />
-    // }
-
-    // 
 
     {
         path: "/", element: <Home />, errorElement: <NotFound />
