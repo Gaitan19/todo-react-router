bc90ff4959a62759438b6d5cdd1d684123c67804
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2023-10-06 13:49:46 -0600

    the suspend was added in the main.jsx

--- prueba-react-routes/src/main.jsx
+++ prueba-react-routes/src/main.jsx
@@ -5,10 +5,16 @@ import './index.css'
 import { router } from './router'
 import { RouterProvider } from 'react-router-dom'
 import { Alert } from './components/Alert'
+import { Suspense } from 'react'
+import LoadingSpinner from './components/LoadingSpinner'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Suspense fallback={<LoadingSpinner />}>
+
     <RouterProvider router={router}/>
+
+    </Suspense>
     <Alert/>
   </React.StrictMode>,
 )
--- prueba-react-routes/src/router/index.jsx
+++ prueba-react-routes/src/router/index.jsx
@@ -8,14 +8,26 @@ const Home = lazy(() => import('../components/pages/Home'));
 const NotFound = lazy(() => import('../components/pages/NotFound'))
 
 export const router = createBrowserRouter([
+    // {
+        // path: "/", element: <Suspense fallback={<LoadingSpinner />}><Home /></Suspense>, errorElement: <NotFound />
+    // },
+    // {
+        // path: "/Login", element: <Suspense fallback={<LoadingSpinner/>}> < Login/> </Suspense> , errorElement: <NotFound />
+    // },
+    // {
+        // path: "/SignUp", element: <Suspense fallback={<LoadingSpinner/>}> <SignUp /> </Suspense> , errorElement: <NotFound />
+    // }
+
+    // 
+
     {
-        path: "/", element: <Suspense fallback={<LoadingSpinner />}><Home /></Suspense>, errorElement: <NotFound />
+        path: "/", element: <Home />, errorElement: <NotFound />
     },
     {
-        path: "/Login", element: <Suspense fallback={<LoadingSpinner/>}> < Login/> </Suspense> , errorElement: <NotFound />
+        path: "/Login", element:  < Login/>  , errorElement: <NotFound />
     },
     {
-        path: "/SignUp", element: <Suspense fallback={<LoadingSpinner/>}> <SignUp /> </Suspense> , errorElement: <NotFound />
+        path: "/SignUp", element: <SignUp />  , errorElement: <NotFound />
     }
 
 ])
